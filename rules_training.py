b398bcac70d85526205c3e46c771d0ee05628319
Adam Kowalczyk <adam.filip.kowalczyk@gmail.com>
2022-06-06 08:38:23 +0200

    Add control print statements

--- rules_training.py
+++ rules_training.py
@@ -30,11 +30,15 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    print(f"Started {args.experiment_name}.", flush=True)
+
     DATA_PATH = args.data_path
 
     raven_train_dataset = RAVENRulesDataset(DATA_PATH, 'train', 4)
     raven_val_dataset = RAVENRulesDataset(DATA_PATH, 'val', 4)
     raven_test_dataset = RAVENRulesDataset(DATA_PATH, 'test', 4)
+
+    print(f"Datasets created.", flush=True)
 
     train_loader = DataLoader(raven_train_dataset, batch_size=args.batch_size, num_workers=args.num_workers)
 
@@ -42,9 +46,13 @@ if __name__ == '__main__':
 
     test_loader = DataLoader(raven_test_dataset, batch_size=args.batch_size, num_workers=args.num_workers)
 
+    print(f"Dataloaders created.", flush=True)
+
     model = SASCL()
 
     model_wr = SASCLRulesLightning(model, args.learning_rate)
+
+    print(f"Models initialized.", flush=True)
 
     experiment_path = os.path.join(args.results_dir, args.experiment_name)
 
@@ -53,9 +61,22 @@ if __name__ == '__main__':
     trainer = pl.Trainer(max_epochs=args.epochs, devices=1, accelerator="auto", 
                         callbacks=[early_stop_callback, model_checkpoint_callback])
 
+    print(f"Trainer with callbacks created.", flush=True)
+
     trainer.fit(model_wr, train_dataloaders=train_loader, val_dataloaders=val_loader)
+
+    print(f"Training finished.", flush=True)
+
+    results_val = trainer.validate(ckpt_path = 'best', dataloaders = val_loader)
+
+    with open(os.path.join(experiment_path, 'train_results.json'), 'w') as f:
+        json.dump(results_val , f)
+
+    print(f"Training validation finished.", flush=True)
 
     results = trainer.validate(ckpt_path = 'best', dataloaders = test_loader)
 
     with open(os.path.join(experiment_path, 'test_results.json'), 'w') as f:
         json.dump(results, f)
+
+    print(f"Testing finished.", flush=True)
